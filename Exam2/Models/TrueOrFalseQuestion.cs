3ef373d998e8d01b23bb57a026c593673790aa96
Philo <philoatefdev@gmail.com>
2024-08-01 07:18:41 +0300

    Creating child class TrueOrFalseQuestion of parent Question

---
+++ Exam2/Models/TrueOrFalseQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    internal class TrueOrFalseQuestion : Question
+    {
+        public TrueOrFalseQuestion(string header, string body, int mark) : base(header, body, mark) 
+        {
+
+        }
+        public override void DisplayQuestion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

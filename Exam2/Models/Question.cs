8f4d491bf0f272d17646860da063d96999ba8439
Philo <philoatefdev@gmail.com>
2024-08-01 07:00:44 +0300

    defining abstract question class to be inhereted

---
+++ Exam2/Models/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    internal abstract class Question
+    {
+
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public int Mark { get; set; }
+
+        public Question(string header, string body, int mark)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+        }
+
+        public abstract void DisplayQuestion();
+    }
+}

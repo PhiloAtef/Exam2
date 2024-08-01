07471d0e6fac7452f9ce5d68191141045ba14995
Philo <philoatefdev@gmail.com>
2024-08-01 07:46:29 +0300

    PracticalExam barebones

---
+++ Exam2/Models/PracticalExam.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    public class PracticalExam : Exam
+    {
+        public PracticalExam(DateTime time, int numQuestions, Subject subject) : base(time, numQuestions, subject) { }
+
+        public override void ShowExam()
+        {
+            foreach (var question in Questions)
+            {
+                question.DisplayQuestion();
+            }
+            Console.WriteLine("Right answers will be shown after finishing the exam.");
+        }
+    }
+}

962a0eccd0b5b02268c4b5e3ba52273d2f5f10ac
Philo <philoatefdev@gmail.com>
2024-08-01 07:40:58 +0300

    creating backbones of basic Exam abstract class

---
+++ Exam2/Models/Exam.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    public abstract class Exam : ICloneable, IComparable<Exam>
+    {
+        public DateTime TimeOfExam { get; set; }
+        public int NumberOfQuestions { get; set; }
+        public List<Question> Questions { get; set; }
+        public Subject ExamSubject { get; set; }
+
+        public Exam(DateTime time, int numQuestions, Subject subject)
+        {
+            TimeOfExam = time;
+            NumberOfQuestions = numQuestions;
+            Questions = new List<Question>();
+            ExamSubject = subject;
+        }
+        public abstract void ShowExam();
+
+        public int CompareTo(Exam? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

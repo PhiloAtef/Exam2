41aaa747efd96a6afbc8521f1f504fee5b21c091
Philo <philoatefdev@gmail.com>
2024-08-01 07:34:23 +0300

    framework of subject class

---
+++ Exam2/Models/Subject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+
+        public Subject(int id, string name)
+        {
+            SubjectId = id;
+            SubjectName = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Subject Id: {SubjectId}, Subject Name: {SubjectName}";
+        }
+    }
+}
